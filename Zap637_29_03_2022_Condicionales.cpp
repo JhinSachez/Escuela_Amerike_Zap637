ce01d5ea990fb56665e3b9ad21a41c607f2904f9
JhinSanchez <81481602+JhinSachez@users.noreply.github.com>
2022-04-05 12:43:27 -0500

    I learn how to made conditionals

---
+++ Zap637_29_03_2022_Condicionales.cpp
@@ -0,0 +1,40 @@
+// 29_03_2022_Condicionales.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    int edad = 0;
+    std::cout << "Ingrese su edad" << std::endl;
+    std::cin >> edad;
+
+    //Condicion If
+   /* if (edad >= 18) {
+        std::cout << "La conoci en un taxi" << std::endl;
+        std::cout << "Se dice que es mayor de edad" << std::endl;
+    }else {
+        std::cout << "No la conoci en un taxi" << std::endl;
+        std::cout << "Ella es menor de edad corre" << std::endl;
+
+    }
+
+    std::cout << "Salio de la condicion..." << std::endl;*/
+
+    /*if (edad <= 18) {
+        std::cout << "Uste es menor vayase" << std::endl;
+    } else if(edad >= 15); {
+        std::cout << "Espere unos años pa entrar" << std::endl;
+    }*/
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
